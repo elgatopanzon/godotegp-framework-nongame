2ff73120d18b6c628cc26bd5b7895b72d8035d3a
ElGatoPanzon <gato@elgatopanzon.io>
2024-01-01 16:07:11 -0600

    Timer: working implementation

--- classes/Godot/Timer.cs
+++ classes/Godot/Timer.cs
@@ -6,29 +6,94 @@
 
 namespace Godot;
 
+using GodotEGP.Logging;
+using GodotEGP.Misc;
+
 public partial class Timer : Node
 {
 	public bool OneShot { get; set; }
-	public int WaitTime { get; set; }
+	public float WaitTime { get; set; }
+	public float TimeLeft { get; set; }
+	public bool Autostart { get; set; }
+
+	private bool _running { get; set; }
+	private bool _ready { get; set; }
 
 	public Timer()
 	{
-		
+		OneShot = false;
+		WaitTime = 0;
+		TimeLeft = 0;
+		Autostart = false;
+
+		_running = false;
 	}
 
 	public bool IsStopped()
 	{
-		return true;
+		return _running == false;
 	}
 
-	public void Start()
+	public void Start(float waitTime = -1)
 	{
-		
+		if (waitTime > 0)
+		{
+			WaitTime = waitTime;
+
+			Stop();
+			Start();
+		}
+
+		LoggerManager.LogDebug("Starting timer", "", "waitTime", WaitTime);
+
+		TimeLeft = WaitTime;
+		_running = true;
 	}
 
 	public void Stop()
 	{
-		
+		LoggerManager.LogDebug("Stopping timer", "", "waitTime", WaitTime);
+
+		TimeLeft = 0;
+	}
+
+	private void Finished()
+	{
+		LoggerManager.LogDebug("Timer finished", "", "waitTime", WaitTime);
+
+		Stop();
+		_running = false;
+
+		EmitSignal(StringNames.Instance["timeout"]);
+
+		if (!OneShot)
+		{
+			Start();
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		// only process if in the scene
+		if (_ready && _running)
+		{
+			TimeLeft -= (float) delta;
+
+			if (TimeLeft <= 0)
+			{
+				Finished();
+			}
+		}
+	}
+
+	public override void _Ready()
+	{
+		_ready = true;
+
+		if (Autostart)
+		{
+			Start();
+		}
 	}
 }
 
