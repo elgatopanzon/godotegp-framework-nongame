56907d635b8bc9871dd62c050106095085ad09f1
ElGatoPanzon <gato@elgatopanzon.io>
2024-01-01 17:04:59 -0600

    RandomNumberGenerator: implementation with seed and incremental-based state restoring

--- classes/Godot/RandomNumberGenerator.cs
+++ classes/Godot/RandomNumberGenerator.cs
@@ -6,29 +6,75 @@
 
 namespace Godot;
 
+using GodotEGP.Logging;
+
 public partial class RandomNumberGenerator : Node
 {
-	public ulong Seed { get; set; }
-	public ulong State { get; set; }
+	private ulong _seed = 0;
+	public ulong Seed
+	{
+		get { return _seed; }
+		set { _seed = value; }
+	}
+
+	private ulong _state;
+	public ulong State
+	{
+		get { return _state; }
+		set { 
+			_state = value;
+
+			_random = new Random((int) _seed);
+			for (int i = 0; i < (int) _state; i++)
+			{
+				_random.NextDouble();
+			}
+		}
+	}
+
+	private Random _random;
 
 	public RandomNumberGenerator()
 	{
-		
-	}
-
-	public void Randomize()
-	{
-		
+		_random = new Random((int) _seed);
 	}
 
 	public float Randf()
 	{
-		return 0;
+		_state++;
+		return (float) _random.NextDouble();
 	}
 
 	public float RandfRange(float from, float to)
 	{
+		_state++;
+		return (float) _random.NextDouble() * (to - from) + from;
+	}
+
+	public int Randfn()
+	{
+		_state++;
+		LoggerManager.LogCritical("Randfn() not implemented");
 		return 0;
 	}
+
+	public int Randi()
+	{
+		_state++;
+		return _random.Next();
+	}
+
+	public int RandiRange(int from, int to)
+	{
+		_state++;
+		return _random.Next(from, to);
+	}
+
+	public void Randomize()
+	{
+		_seed = (ulong) Guid.NewGuid().GetHashCode();
+		_random = new Random((int) _seed);
+	}
+
 }
 
